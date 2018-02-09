34dd0a8bd8d0eca4708da55eb6655f04d9c161ac
Guillaume Liegard <guillaume.liegard@ign.fr>
2018-02-09 18:18:45 +0100

    WIP

--- examples/globe_paris_1_oriented_camera.js
+++ examples/globe_paris_1_oriented_camera.js
@@ -42,8 +42,16 @@ promises.push(itowns.Fetcher.json('./layers/JSONLayers/Ortho.json').then(addLaye
 promises.push(itowns.Fetcher.json('./layers/JSONLayers/WORLD_DTM.json').then(addLayerCb));
 promises.push(itowns.Fetcher.json('./layers/JSONLayers/IGN_MNT_HIGHRES.json').then(addLayerCb));
 
+
+var layer = {
+    orientationType: 'Stereopolis2',
+    sensors: [],
+}
+
 function orientedImagesInit(orientedImages) {
+    console.log('SENSORS : ', layer.sensors);
     var i;
+    var j = 0;
     var ori;
     var axis;
     var camera;
@@ -72,6 +80,13 @@ function orientedImagesInit(orientedImages) {
         camera.lookAt(new itowns.THREE.Vector3(0, 1, 0));
         axis.add(camera);
 
+        for (j = 0; j < layer.sensors.length; j++) {
+            var sensor = layer.sensors[j];
+            var sensorHelper = new itowns.THREE.AxesHelper(0.5);
+            sensorHelper.position.sub(sensor.centerCameraInPano);
+            axis.add(sensorHelper);
+        }
+
         // add axis to scene and update matrix world
         globeView.scene.add(axis);
         axis.updateMatrixWorld();
@@ -80,11 +95,117 @@ function orientedImagesInit(orientedImages) {
         cameraHelper = new itowns.THREE.CameraHelper(camera);
         globeView.scene.add(cameraHelper);
         cameraHelper.updateMatrixWorld(true);
+
+        // break;
     }
 }
 
-itowns.Fetcher.json('http://www.itowns-project.org/itowns-sample-data/panoramicsMetaData.json',
-{ crossOrigin: '' }).then(orientedImagesInit);
+var THREE = itowns.THREE;
+
+function getMatrix4FromRotation(Rot) {
+    var M4 = new THREE.Matrix4();
+    M4.elements[0] = Rot.elements[0];
+    M4.elements[1] = Rot.elements[1];
+    M4.elements[2] = Rot.elements[2];
+    M4.elements[4] = Rot.elements[3];
+    M4.elements[5] = Rot.elements[4];
+    M4.elements[6] = Rot.elements[5];
+    M4.elements[8] = Rot.elements[6];
+    M4.elements[9] = Rot.elements[7];
+    M4.elements[10] = Rot.elements[8];
+    return M4;
+}
+
+// initialize a sensor for each camera and create the material (and the shader)
+function sensorsInit(res) {
+    let i;
+    console.log(res);
+    var withDistort = false;
+    for (const s of res) {
+        var sensor = {};
+        sensor.id = s.id;
+
+        var rotCamera2Pano = new THREE.Matrix3().fromArray(s.rotation);
+        var rotTerrain = new THREE.Matrix3().set(
+            1, 0, 0,
+            0, 1, 0,
+            0, 0, 1);
+        if (layer.orientationType && (layer.orientationType == 'Stereopolis2')) {
+            rotTerrain = new THREE.Matrix3().set(
+                0, -1, 0,
+                1, 0, 0,
+                0, 0, 1);
+        }
+        var rotEspaceImage = new THREE.Matrix3().set(
+            1, 0, 0,
+            0, 1, 0,
+            0, 0, 1);
+        rotCamera2Pano = rotTerrain.clone().multiply(rotCamera2Pano.clone().multiply(rotEspaceImage));
+        var rotPano2Camera = rotCamera2Pano.clone().transpose();
+
+        
+
+
+        var centerCameraInPano = new THREE.Vector3().fromArray(s.position);
+        sensor.centerCameraInPano = centerCameraInPano;
+
+
+        var transPano2Camera = new THREE.Matrix4().makeTranslation(
+            -centerCameraInPano.x,
+            -centerCameraInPano.y,
+            -centerCameraInPano.z);
+
+
+
+
+        var projection = (new THREE.Matrix3().fromArray(s.projection)).transpose();
+        var rotPano2Texture = projection.clone().multiply(rotPano2Camera);
+        sensor.mp2t = getMatrix4FromRotation(rotPano2Texture).multiply(transPano2Camera);
+        // sensor.rotPano2Texture = rotPano2Texture;
+        // sensor.centerCameraInPano = centerCameraInPano;
+        sensor.distortion = null;
+        sensor.pps = null;
+        if (s.distortion) {
+            sensor.pps = new THREE.Vector2().fromArray(s.distortion.pps);
+            var disto = new THREE.Vector3().fromArray(s.distortion.poly357);
+            sensor.distortion = new THREE.Vector4(disto.x, disto.y, disto.z, s.distortion.limit * s.distortion.limit);
+            if (s.distortion.l1l2) {
+                sensor.l1l2 = new THREE.Vector2().fromArray(s.distortion.l1l2);
+                sensor.etats = s.distortion.etats;
+            }
+            else {
+                sensor.l1l2 = new THREE.Vector2().set(0, 0);
+                sensor.etats = 0;
+            }
+            withDistort = true;
+        }
+        sensor.size = new THREE.Vector2().fromArray(s.size);
+        layer.sensors.push(sensor);        
+    }
+    console.log(layer.sensors[0].mp2t);
+}
+
+// itowns.Fetcher.json('http://www.itowns-project.org/itowns-sample-data/panoramicsMetaData.json',
+// { crossOrigin: '' }).then(orientedImagesInit);
+
+// function calibrationInit(cameras) {
+//     console.log(cameras);
+// }
+
+// itowns.Fetcher.json('http://localhost:8080/examples/cameraCalibration.json',
+// { crossOrigin: '' }).then(sensorsInit);
+
+
+var promises = [];
+
+promises.push(itowns.Fetcher.json('http://localhost:8080/examples/cameraCalibration.json', { crossOrigin: '' }));
+promises.push(itowns.Fetcher.json('http://www.itowns-project.org/itowns-sample-data/panoramicsMetaData.json', { crossOrigin: '' }));
+
+Promise.all(promises).then((res) => { 
+    sensorsInit(res[0]); 
+    orientedImagesInit(res[1]); 
+    
+});
 
 exports.view = globeView;
 exports.initialPosition = positionOnGlobe;
